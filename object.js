3df138cebbb25c8aba4a8b4789436793b425df5f
Noman Mia <nomanislamnishad@gmail.com>
2025-01-31 15:54:14 +0600

    added object in js oop

--- object.js
+++ object.js
@@ -1,13 +1,26 @@
 //create object
 //How to create object using object literal
-let person = {
-    firstName : "NoMan",
-    lastName : "khan",
-    age : 21,
-    city : "RangPur",
-    isBangladeshi : true,
-    getName : (()=>{
-        return `my first name is ${person.firstName}`
-    })
-}
-console.log(person.getName);
+// let person = {
+//     firstName: "NoMan",
+//     lastName: "Khan",
+//     age: 21,
+//     city: "RangPur",
+//     isBangladeshi: true,
+//     getName: function() {
+//         return `My first name is ${this.firstName}`;
+//     }
+// };
+
+// console.log(person.getName()); 
+
+
+//by creating instance of object
+let man = new Object();
+man.firstName = "NoMan";
+man.lastName = "Khan";
+man.age = 21;
+man.getAge = () => {
+    return `My age is ${man.age}`; 
+};
+
+console.log(man.getAge()); 
