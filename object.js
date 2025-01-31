78a46b3ddfd3660b21a510736cdd18bb43004a24
Noman Mia <nomanislamnishad@gmail.com>
2025-01-31 15:30:35 +0600

    first commit

---
+++ object.js
@@ -0,0 +1,12 @@
+//create object
+//How to create object using object literal
+let person = {
+    firstName : "NoMan",
+    lastName : "khan",
+    age : 21,
+    city : "RangPur",
+    isBangladeshi : true,
+    getName : (()=>{
+        return `my first name is ${person.firstName}`
+    })
+}
